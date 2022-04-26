5f391799b94b028b8d730d1a67fbfd6e438f4d77
Claire Tsai <claire.tsai@onelab.tw>
2022-04-26 21:32:42 +0800

    改為貼文形式

--- server.js
+++ server.js
@@ -1,78 +1,93 @@
-const http = require('http');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const header = require('./header');
-const Todo = require('./Models/Todo')
-const {successHandle, errorHandle} = require('./handle')
+const http = require('http')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const headers = require('./header')
+const Post = require('./Models/Post')
+const { successHandle, errorHandle } = require('./handle')
 
-dotenv.config({path: './config.env'})
+dotenv.config({ path: './config.env' })
 
-const DB = process.env.DATABASE.replace('<password>',process.env.DATABASE_PASSWORD)
-mongoose.connect(DB)
-.then(()=>{
-  console.log('資料庫連線成功')
-}).catch(error=>{
-  console.log(error)
-})
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD)
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log('資料庫連線成功')
+  })
+  .catch((error) => {
+    console.log(error)
+  })
 
-const requestListener = async (req,res) => {
+const requestListener = async (req, res) => {
   let body = ''
-  req.on('data', chunk => {
+  req.on('data', (chunk) => {
     body += chunk
   })
-  if(req.url == '/posts' && req.method == 'GET'){
-    const todo = await Todo.find()
-    successHandle(res,todo)
-  }else if(req.url == '/posts' && req.method == 'POST') {
-    req.on('end', async ()=> {
-      try{
-        const title = JSON.parse(body).title
-        const todo = await Todo.create({
-          title
+  if (req.url == '/posts' && req.method == 'GET') {
+    const post = await Post.find()
+    successHandle(res, post)
+  } else if (req.url == '/posts' && req.method == 'POST') {
+    req.on('end', async () => {
+      try {
+        const data = JSON.parse(body)
+        const post = await Post.create({
+          name: data.name,
+          content: data.content,
+          image: data.image,
+          createdAt: data.createdAt,
+          likes: data.likes,
         })
-        successHandle(res,todo)
-      }catch(error){
-        errorHandle(res,error,'欄位填寫錯誤')
+        successHandle(res, post)
+      } catch (error) {
+        errorHandle(res, error, '欄位填寫錯誤')
       }
     })
-  }else if(req.url == '/posts' && req.method == 'DELETE') {
-    await Todo.deleteMany({})
-    successHandle(res,[],'刪除全部成功')
-  }else if(req.url.startsWith('/posts/') && req.method == 'DELETE') {
-
-    try{
+  } else if (req.url == '/posts' && req.method == 'DELETE') {
+    await Post.deleteMany({})
+    successHandle(res, [], '刪除全部成功')
+  } else if (req.url.startsWith('/posts/') && req.method == 'DELETE') {
+    try {
       const id = req.url.split('/').pop()
-      const todo = await Todo.findByIdAndDelete(id)
-      successHandle(res,[],'刪除成功')
-    }catch(error){
-      errorHandle(res,error,'查無此 id')
+      const post = await Post.findByIdAndDelete(id)
+      if (post) {
+        successHandle(res, post, '刪除成功')
+      } else {
+        throw new Error()
+      }
+    } catch (error) {
+      errorHandle(res, error, '查無此 id')
     }
-  }else if(req.url.startsWith('/posts/') && req.method == 'PATCH') {
-    req.on('end', async ()=> {
-      try{
+  } else if (req.url.startsWith('/posts/') && req.method == 'PATCH') {
+    req.on('end', async () => {
+      try {
         const id = req.url.split('/').pop()
-        const editTitle = JSON.parse(body).title
-        if(editTitle != undefined){
-          const todo = await Todo.findByIdAndUpdate(id, {title: editTitle}, {returnDocument:'after'})
-          successHandle(res,todo,'更新成功')
-        }else {
-          throw new Error('Title 欄位為必填欄位')
+        const data = JSON.parse(body)
+        const post = await Post.findByIdAndUpdate(
+          id,
+          { name: data.name, content: data.content, image: data.image, createdAt: data.createdAt, likes: data.likes },
+          { returnDocument: 'after' }
+        )
+        if (post) {
+          successHandle(res, post, '更新成功')
+        } else {
+          throw new Error('無此ID或欄位填寫錯誤')
         }
-      }catch(error){
-        errorHandle(res,error,error.message)
+      } catch (error) {
+        errorHandle(res, error, error.message)
       }
     })
-  }else if(req.method == 'OPTIONS') {
+  } else if (req.method == 'OPTIONS') {
     res.writeHead(200, headers)
     res.end()
-  }else {
+  } else {
     res.writeHead(404, headers)
-    res.write(JSON.stringify({
-      status: 'false',
-      message: '無此網路路由'
-    }))
+    res.write(
+      JSON.stringify({
+        status: 'false',
+        message: '無此網路路由',
+      })
+    )
     res.end()
   }
 }
 const server = http.createServer(requestListener)
-server.listen(process.env.PORT);
+server.listen(process.env.PORT)
